788558d78e3a2710997b37e226ea9c9f143bc9d9
ChristineWasike <christinewasike22@gmail.com>
2020-10-07 14:15:12 +0300

    Refactored the sellectAll and selectOne functions by introducing the execute query function

--- app/database/db.php
+++ app/database/db.php
@@ -1,37 +1,68 @@
 <?php
-require ('connect.php');
+require('connect.php');
 
 function displayData($value){
     echo "<pre>", print_r($value, true), "<pre>";
 }
 
+
+function executeQuery($sql, $data){
+    global $conn;
+    $stmt= $conn->prepare($sql);
+    $values = array_values($data);
+    $types = str_repeat('s', count($values));
+    $stmt->bind_param($types, ...$values);
+    $stmt->execute();
+    return $stmt;
+}
+
 function selectAll($table, $conditions = []){
     global $conn;
     $sql = "SELECT * FROM $table";
-    if (empty($conditions)){
+    if (empty($conditions)) {
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-    } else{
+    } else {
         // return records that match conditions
 
         $index = 0;
-        foreach ($conditions as $key => $value){
-            if ($index == 0){
+        foreach ($conditions as $key => $value) {
+            if ($index == 0) {
                 $sql = $sql . " WHERE $key = ?";
             } else {
                 $sql = $sql . " AND $key = ?";
             }
-            $index ++;
+            $index++;
         }
         displayData($sql);
-        $stmt = $conn->prepare($sql);
-        $values = array_values($conditions);
-        $types = str_repeat('s', count($values));
-        $stmt->bind_param($types, ...$values);
-        $stmt->execute();
+        $stmt = executeQuery($sql, $conditions);
         return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
     }
+
+}
+
+function selectOne($table, $conditions){
+    global $conn;
+    $sql = "SELECT * FROM $table";
+    // return records that match conditions
+
+    $index = 0;
+    foreach ($conditions as $key => $value) {
+        if ($index == 0) {
+            $sql = $sql . " WHERE $key = ?";
+        } else {
+            $sql = $sql . " AND $key = ?";
+        }
+        $index++;
+    }
+
+    // Promotes efficiency by preventing searching
+    // through all records in the database
+    $sql = $sql . " LIMIT 1";
+
+    $stmt = executeQuery($sql, $conditions);
+    return $stmt->get_result()->fetch_assoc();
 
 }
 
@@ -40,9 +71,9 @@ $conditions = [
     'username' => 'NjiMbitaownu'
 ];
 
-$users = selectAll('users');
+//$users = selectAll('users');
 
-$specific_users = selectAll('users', $conditions);
+$specific_users = selectOne('users', $conditions);
 
 displayData($specific_users);
 
