44e08c474d4221ac300609a9689f90565fb08c69
ChristineWasike <christinewasike22@gmail.com>
2021-10-26 11:41:18 +0300

    Page updates

--- about.php
+++ about.php
@@ -15,50 +15,50 @@ include("path.php");
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
-    
-    <!-- Open Sans Link -->
+
+    <!-- Lora Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/index.css">
     <link rel="stylesheet" href="assets/css/about.css">
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark">
-  <a class="navbar-brand" href="index.php">
-      <picture>
-      <source media="(min-width:650px)" srcset="assets/img/mwl_logo.png">
-      <img src="assets/img/mws_logo.png" alt="...">
-      </picture>
-    </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="index.php">
+            <picture>
+                <source media="(min-width:650px)" srcset="assets/img/mwl_logo.png">
+                <img src="assets/img/mws_logo.png" alt="...">
+            </picture>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="resume.php">Services</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://blog.mbitaownu.com/blog/">Blog</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact</a>
-      </li>
-  
-    </ul>
-  </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="about.php">About <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="resume.php">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://blog.mbitaownu.com/blog/">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
 
     <div class="container-fluid" id="main">
 
@@ -66,7 +66,7 @@ include("path.php");
         <section id="about-me">
             <div class="row about-me">
                 <div class="col-md-6 image">
-                    <img src="assets/img/about_me.jpg" alt="nji" id="about_image">
+                    <img src="assets/img/about_me.png" alt="nji" id="about_image">
                 </div>
 
                 <!-- animate__animated animate__fadeInRight -->
--- index.php
+++ index.php
@@ -20,15 +20,10 @@ $posts = getPublishedPosts();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
 
-    <!-- Open Sans Link -->
+    <!-- Lora Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-
-    <!-- Arvo Link -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Arvo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/common.css">
