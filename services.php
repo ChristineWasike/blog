4a7fc73819c93905bf19833b97f6198a3bb0e0ed
ChristineWasike <christinewasike22@gmail.com>
2021-12-22 15:19:07 +0300

    Changed to services page

---
+++ services.php
@@ -0,0 +1,257 @@
+<?php
+include("path.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services | Nji Mbitaownu</title>
+    <link rel="shortcut icon" href="assets/img/shortcut_icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Lora Link -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/services.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <a class="navbar-brand" href="index.php">
+            <picture>
+                <source media="(min-width:650px)" srcset="assets/img/mwl_logo.png">
+                <img src="assets/img/mws_logo.png" alt="...">
+            </picture>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="services.php">Services <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://blog.mbitaownu.com">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="landing">
+                <div style="color: white;" class="col-md-6 landing_content">
+                    <div class="desc">
+                        <h2>Services</h2>
+                        <h4>What Can I do For You?</h4>
+                        <p>My aim is to make your business scalable and run
+                            efficiently to its full potential. I want to join forces
+                            with you to create the venture you have always
+                            envisioned and dreamed about, have it come to life
+                            and make it a reality for you. A business that
+                            people will be in awe of and will not stop talking
+                            about because you are breaking barriers, defying
+                            odds and turning negatives into positives. Are you
+                            ready to dive in and work with me?</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row services">
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services1">
+                        <h5 class="card-title">Entrepreneurial <br>Finance</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">A startup service where I assist new
+                            emerging businesses through the new
+                            venture development process. As a coach
+                            and trainer I take them through each step
+                            and evaluate their progress and fine tune
+                            their business idea into a market ready
+                            venture which is solving a problem in
+                            society. This is a six month long
+                            bootcamp designed to launch businesses. <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services2">
+                        <h5 class="card-title">New Venture <br>Development</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">A startup service where I assist new
+                            emerging businesses through the new
+                            venture development process. As a coach
+                            and trainer I take them through each step
+                            and evaluate their progress and fine tune
+                            their business idea into a market ready
+                            venture which is solving a problem in
+                            society. This is a six month long
+                            bootcamp designed to launch businesses. <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services3">
+                        <h5 class="card-title">Incubator <br>Development</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">I offer this service to incubator
+                            programmes or organisations that want
+                            or host incubators for businesses and
+                            startups. Where I work with them as an
+                            incubator developer and give them a
+                            blueprint document of how the
+                            programme is to be structured, with what
+                            resources and how to manage them. I
+                            also manage the program if institutions
+                            want that service added too <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services4">
+                        <h5 class="card-title">Facilitation</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">A startup service where I assist new
+                            emerging businesses through the new
+                            venture development process. As a coach
+                            and trainer I take them through each step
+                            and evaluate their progress and fine tune
+                            their business idea into a market ready
+                            venture which is solving a problem in
+                            society. This is a six month long
+                            bootcamp designed to launch businesses. <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services5">
+                        <h5 class="card-title">Business <br>Research</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">A startup service where I assist new
+                            emerging businesses through the new
+                            venture development process. As a coach
+                            and trainer I take them through each step
+                            and evaluate their progress and fine tune
+                            their business idea into a market ready
+                            venture which is solving a problem in
+                            society. This is a six month long
+                            bootcamp designed to launch businesses. <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 card-shell">
+                <div class="card text-white" style="width: 22rem;">
+                    <div class="card-body card_image" id="services6">
+                        <h5 class="card-title">Startup <br>Fundraising</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">A startup service where I assist new
+                            emerging businesses through the new
+                            venture development process. As a coach
+                            and trainer I take them through each step
+                            and evaluate their progress and fine tune
+                            their business idea into a market ready
+                            venture which is solving a problem in
+                            society. This is a six month long
+                            bootcamp designed to launch businesses. <br><a href="#">Learn more</a></p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row project">
+            <div class="form col-md-6 offset-3">
+                <h2>Have A Project? Let's Talk</h2>
+                <form action="https://formspree.io/f/xjvjyjln" method="POST">
+                    <div class="form-group">
+                        <!-- <label for="fullName">Full Name</label> -->
+                        <input name="name" type="name" class="form-control" id="fullName" aria-describedby="nameHelp" placeholder="Full name">
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="exampleInputEmail1">Email address</label> -->
+                        <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">All emails are kept private.</small>
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="message">Your message</label> -->
+                        <textarea name="message" class="form-control" id="message" rows="3" placeholder="Your message"></textarea>
+                    </div>
+                    <button type="submit" id="project_button" class="btn btn-dark">Submit</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Footer -->
+    <?php include(ROOT_PATH . "/app/includes/footer.php") ?>
+    <!-- Footer -->
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script src="assets/vendor/counterup/counterup.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/venobox/venobox.min.js"></script>
+    <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="assets/vendor/typed.js/typed.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+
+
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
