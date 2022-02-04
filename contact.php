505964838a6c7576edd9bcd2860170428e51fa08
ChristineWasike <christinewasike22@gmail.com>
2022-02-04 14:27:18 +0300

    Contact page done

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ include("path.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
+ 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -62,6 +62,7 @@ include("path.php");
 
   <div class="container-fluid">
     <h2>Get in Touch</h2>
+
     <div class="row landing">
       <div class="form col-md-6">
         <form action="https://formspree.io/f/xjvjyjln" method="POST">
